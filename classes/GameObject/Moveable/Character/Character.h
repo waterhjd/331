093abdb29b5beed6eb2e96c02f2b21437d8b997e
Daniel Christiaan Deyaegher (Danny) <deyaegdc@stumail.csci.viu.ca>
2016-02-02 02:04:41 -0800

    Moved coords, speed vars into Moveable protected.

--- classes/GameObject/Moveable/Character/Character.h
+++ classes/GameObject/Moveable/Character/Character.h
@@ -8,17 +8,8 @@ using namespace std;
 
 class Character: public Moveable {
    private:
-      int m_position_x;
-			int m_position_y;
-			int m_width;
-      int m_speed_x;
-      int m_speed_y;
-      int m_speed;      
-			int m_jumping;
+    bool m_jumping;
    public:
-    int getX() {return m_position_x;};
-    int getY() {return m_position_y;};
-    int getR() {return m_width;};
     // void bounceX() {m_speed_x *= -1;};
     // void bounceY() {m_speed_y *= -1;};
 		void stopY() {m_speed_y = 0;};
