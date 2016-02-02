bd0e7c847c8c48ce9e4fc7d7bf0e885945196071
Daniel Christiaan Deyaegher (Danny) <deyaegdc@stumail.csci.viu.ca>
2016-02-02 03:51:49 -0800

    Testing out game logic. Moved character and bunny things around

--- classes/GameObject/Moveable/Bunny/Bunny.h
+++ classes/GameObject/Moveable/Bunny/Bunny.h
@@ -6,17 +6,20 @@
 
 class Bunny : public Moveable {
    private:
+   /*
    int m_position_x;
    int m_position_y;
    int m_width;
-   
+   */
+   bool disp;
+   /*
    int m_speed_x;
    int m_speed_y;
    int m_speed;
-   
+   */
    int m_health;
    public:
-   
+/*   
    // Get Bunny Position
    int getX() {return m_position_x;};
    
@@ -25,13 +28,13 @@ class Bunny : public Moveable {
    
    // Get bunny size
    int getWidth() {return m_width;};
-   
+*/   
    void display();
    void update();
    void move();
    
    // Bunny hits character send character postion to check
-   bool collide(Character c);
+   bool collide(Character *c);
    //  Player Character projectile send projectile to check
    //bool zapped(Lightning L);
    
