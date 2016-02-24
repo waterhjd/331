438b2fdb09dd5f7154a40879ae4bf3e2bcffd8f4
adriaanwm <adriaan.mulder@gmail.com>
2016-02-24 15:12:14 -0800

    cleanup

--- classes/GameObject/Moveable/Character/Character.cpp
+++ classes/GameObject/Moveable/Character/Character.cpp
@@ -9,44 +9,30 @@ void Character::display()
 							m_width);
 }
 
-// void Character::update()
-// { 
-// 		m_speed_y -= Gravity;
-// 		m_position_y += m_speed_y * m_speed;
-//     m_position_x += m_speed_x * m_speed;
-// 		
-// }
 
-void Character::jump()
-{
+void Character::jump() {
 	if (!m_jumping) {
 		m_speed_y = 20;
 		m_jumping = true;
 	}
-	// if (m_speed_y == 0) {
-	// 	m_speed_y = 3 ;
-	// }
 }
 
-void Character::right()
-{
+void Character::right() {
 	m_position_x += 1;	
-   m_speed_x = 1;
+  m_speed_x = 1;
 }
 
-void Character::left()
-{
+void Character::left() {
 	m_position_x -= 1;	
-   m_speed_x = -1;
+  m_speed_x = -1;
 }
 
-Character::Character()
-{
-   m_speed_x = 0;
-   m_speed_y = -1;
-   m_speed = 1;
-   m_position_x = 200;
-   m_position_y = 250;
-   m_width = 20;
-   m_jumping = true;
+Character::Character() {
+  m_speed_x = 0;
+  m_speed_y = -1;
+  m_speed = 1;
+  m_position_x = 200;
+  m_position_y = 250;
+  m_width = 20;
+  m_jumping = true;
 }
