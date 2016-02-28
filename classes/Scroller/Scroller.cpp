5bb0613593aa2f0a63166eb12ac458a9c0187c8c
adriaanwm <adriaan.mulder@gmail.com>
2016-02-27 21:34:00 -0800

    cleanup

--- classes/Scroller/Scroller.cpp
+++ classes/Scroller/Scroller.cpp
@@ -1,11 +1,24 @@
 #include "Scroller.h"
 
 void Scroller::update() {
+	int character_pos_minus_center;
+	int window_center = m_window_width / 2;
+
+	// make sure character doesn't move past left border
 	m_left_border->collide(m_character);
-	if (m_x_distance < m_character->get_left() - (m_window_width / 2)) {
-		m_x_distance = m_character->get_left() - (m_window_width / 2);
+
+	// calculate m_x_distance so that it only increases
+	// when character tries to move into right half of screen
+	character_pos_minus_center = m_character->get_left() - window_center;
+	if (m_x_distance < character_pos_minus_center) {
+		m_x_distance = character_pos_minus_center;
 	}
+
+	// keep left border updated as the camera moves right
 	m_left_border->update(m_x_distance);
+
+
+	// set camera to be centered at m_x_distance
 	glTranslatef(- m_x_distance, 0.0, 0.0);
 }
 
--- classes/Scroller/Scroller.h
+++ classes/Scroller/Scroller.h
@@ -9,6 +9,13 @@ class Scroller {
 	private:
 		Character *m_character;
 		Border *m_left_border;
+
+		/** 
+		 * m_x_distance tracks how for along the x axis
+		 * the player has travelled.
+		 * In terms of the camera view, it is always
+		 * at the left edge of the window
+		 */
 		int m_x_distance;
 		int m_window_width;
 	public:
