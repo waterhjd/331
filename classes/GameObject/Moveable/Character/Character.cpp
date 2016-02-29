44e3c3573bd2f3a51fcef0296bc605945f1e1fbd
adriaanwm <adriaan.mulder@gmail.com>
2016-02-28 17:06:37 -0800

    revert to old version

--- classes/GameObject/Moveable/Character/Character.cpp
+++ classes/GameObject/Moveable/Character/Character.cpp
@@ -1,25 +1,14 @@
 #include "Character.h"
-#include "../../../ImageLoader/ImageLoader.h"
 
 void Character::display()
 {  
-   /*
-   m_height = m_text_height[m_currImg];
-   m_width = m_text_width[m_currImg];
-   
-   glEnable(GL_TEXTURE_2D);
-   glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE,GL_REPLACE);
-   
-   glBindTexture (GL_TEXTURE_2D, m_textures[m_currImg]);
-*/
-   glColor3fv(colours[4]); 
-   ImageLoader::rectangle(m_position_x, 
-                          m_position_y, 
-                          m_width, 
-                          m_height);
-   //glDisable(GL_TEXTURE_2D);
-
+    glColor3fv(colours[4]);
+    rectangle(m_position_x, 
+							m_position_y, 
+							m_width, 
+							m_width);
 }
+
 
 void Character::jump() {
 	if (!m_jumping) {
@@ -39,8 +28,6 @@ void Character::left() {
 }
 
 Character::Character() {
-  //m_textures[0] = ImageLoader::LoadTexture("images/cockroach_right.bmp", m_text_width[0], m_text_height[0]);
-  m_currImg = 0;
   m_speed_x = 0;
   m_speed_y = -1;
   m_speed = 1;
